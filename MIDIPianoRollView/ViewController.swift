8268ba7651d1517b5122323dd92f06de70168563
cemolcay <ccemolcay@gmail.com>
2018-11-26 14:36:19 +0200

    Initial Commit

---
+++ MIDIPianoRollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MIDIPianoRollView
+//
+//  Created by cem.olcay on 26/11/2018.
+//  Copyright © 2018 cemolcay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
