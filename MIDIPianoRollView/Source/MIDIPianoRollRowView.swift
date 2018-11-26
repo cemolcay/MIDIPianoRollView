e6ddd9246f86a19cb7b46c1d30f099d76419a4cd
cemolcay <ccemolcay@gmail.com>
2018-11-26 16:14:51 +0200

    Add zoom level
    Add basic row view implementation

--- MIDIPianoRollView/Source/MIDIPianoRollRowView.swift
+++ MIDIPianoRollView/Source/MIDIPianoRollRowView.swift
@@ -7,8 +7,56 @@
 //
 
 import UIKit
+import ALKit
+import MusicTheorySwift
 
 /// Represents a MIDI note row of the `MIDIPianoRollView`.
 public class MIDIPianoRollRowView: UIView {
+  /// Pitch of the row.
+  public var pitch: Pitch
+  /// Label of the pitch.
+  public var pitchLabel = UILabel()
+  /// Minimum font size of the pitch label.
+  public var maxFontSize: CGFloat = 17
+  /// Maximum font size of the pitch label.
+  public var minFontSize: CGFloat = 11
 
+  // MARK: Init
+
+  /// Initilizes the row with an assigned MIDI pitch.
+  ///
+  /// - Parameter pitch: MIDI pitch of the row.
+  public init(pitch: Pitch) {
+    self.pitch = pitch
+    super.init(frame: .zero)
+    commonInit()
+  }
+
+  /// Initilizes the row from a coder with 0 pitch value.
+  ///
+  /// - Parameter aDecoder: Decoder.
+  public required init?(coder aDecoder: NSCoder) {
+    self.pitch = Pitch(midiNote: 0)
+    super.init(coder: aDecoder)
+    commonInit()
+  }
+
+  /// Default init function.
+  private func commonInit() {
+    // Setup pitch label
+    pitchLabel.textAlignment = .center
+    addSubview(pitchLabel)
+    translatesAutoresizingMaskIntoConstraints = false
+    pitchLabel.translatesAutoresizingMaskIntoConstraints = false
+    pitchLabel.fill(to: self)
+    // Setup default coloring
+    backgroundColor = pitch.key.accidental == .natural ? UIColor.white : UIColor.black
+    pitchLabel.textColor = pitch.key.accidental == .natural ? UIColor.black : UIColor.white
+  }
+
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+    let fontSize = min(maxFontSize, max(minFontSize, frame.size.height - 4))
+    pitchLabel.font = pitchLabel.font.withSize(fontSize)
+  }
 }
--- MIDIPianoRollView/Source/MIDIPianoRollView.swift
+++ MIDIPianoRollView/Source/MIDIPianoRollView.swift
@@ -11,6 +11,32 @@ import ALKit
 import MusicTheorySwift
 import MIDIEventKit
 
+/// Vertical line layer for the each beat of the measure on the `MIDIPianoRollView`.
+public class MIDIPianoRollMeasureLineLayer: CALayer {
+  /// Position on the measure in beats.
+  public var measurePosition: Double = 0
+  /// Is visible or not.
+  public var isVisible: Bool = true
+
+  /// Initilizes the line with the position on the measure and visibility.
+  ///
+  /// - Parameters:
+  ///   - measurePosition: Position on the measure.
+  ///   - isVisible: 
+  init(measurePosition: Double, isVisible: Bool = true) {
+    self.measurePosition = measurePosition
+    self.isVisible = true
+    super.init()
+  }
+
+  /// Initilizes with a coder.
+  ///
+  /// - Parameter aDecoder: Coder.
+  public required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+}
+
 /// Piano roll with customisable row count, row range, beat count and editable note cells.
 public class MIDIPianoRollView: UIScrollView {
 
@@ -20,6 +46,44 @@ public class MIDIPianoRollView: UIScrollView {
     case fixed(Int)
     /// Always a bar more than the last note's bar.
     case auto
+  }
+
+  /// Zoom level of the piano roll that showing the mininum amount of beat.
+  public enum ZoomLevel: Double {
+    /// Whole bar.
+    case whole = 1.0
+    /// Half of a bar.
+    case half = 0.5
+    /// Quarter of a bar.
+    case quarter = 0.25
+    /// Eighth of a bar.
+    case eighth = 0.125
+    /// Sixteenth of a bar.
+    case sixteenth = 0.0625
+    /// Thirtysecond of a bar.
+    case thirtysecond = 0.03125
+    /// Sixtyfourth of a bar.
+    case sixtyfourth = 0.015625
+
+    /// Corresponding note value for the zoom level.
+    public var noteValue: NoteValue {
+      switch self {
+      case .whole:
+        return NoteValue(type: .whole)
+      case .half:
+        return NoteValue(type: .half)
+      case .quarter:
+        return NoteValue(type: .quarter)
+      case .eighth:
+        return NoteValue(type: .eighth)
+      case .sixteenth:
+        return NoteValue(type: .sixteenth)
+      case .thirtysecond:
+        return NoteValue(type: .thirtysecond)
+      case .sixtyfourth:
+        return NoteValue(type: .sixtyfourth)
+      }
+    }
   }
 
   /// All notes in the piano roll.
@@ -33,16 +97,22 @@ public class MIDIPianoRollView: UIScrollView {
   /// Enables/disables the edit mode. Defaults false.
   public var isEditing: Bool = false
 
+  /// Current `ZoomLevel` of the piano roll.
+  public var zoomLevel: ZoomLevel = .quarter
   /// Maximum width of a beat on the measure, max zoomed in.
   public var maxMeasureWidth: CGFloat = 120
   /// Minimum width of a beat on the measure, max zoomed out.
   public var minMeasureWidth: CGFloat = 50
+  /// Current with of a beat on the measure.
+  public var measureWidth: CGFloat = 90
   /// Fixed height of the measure on the top.
   public var measureHeight: CGFloat = 40
-  /// Left hand side row width on the piano roll.
+  /// Fixed left hand side row width on the piano roll.
   public var rowWidth: CGFloat = 120
   /// Fixed height of a row on the piano roll.
   public var rowHeight: CGFloat = 40
+  /// Label configuration for the measure beat labels.
+  public var measureLabelConfig = UILabel()
 
   /// Enables/disables the zooming feature. Defaults true.
   public var isZoomingEnabled: Bool = true
@@ -51,9 +121,24 @@ public class MIDIPianoRollView: UIScrollView {
   /// Enables/disables the multiple note cell editing at once. Defaults true.
   public var isMultipleEditingEnabled: Bool = true
 
+  /// Reference of the all labels of the measure
+  private var measureBeatLabels: [UILabel] = []
+  /// Reference of the all row views.
+  private var rowViews: [MIDIPianoRollRowView] = []
+  /// Reference of the all cell views.
+  private var cellViews: [MIDIPianoRollCellView] = []
+  /// Reference of the all horizontal row lines.
+  private var rowLines: [CALayer] = []
+  /// Reference of the all vertical measure beat lines.
+  private var measureLines: [MIDIPianoRollMeasureLineLayer] = []
+
   /// Renders the piano roll.
   public override func layoutSubviews() {
     super.layoutSubviews()
+
+    // References of the current width and height for laying out.
+    var currentWidth: CGFloat = 0
+    var currentHeight: CGFloat = 0
 
 
   }
